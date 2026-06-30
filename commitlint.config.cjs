9d1ab244efe57a8101a26224ebef5200738fa901
Elliot Chen <2340896+cyfyifanchen@users.noreply.github.com>
2026-06-30 08:19:03 +0800

    ci: make commitlint config self contained

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -12,7 +12,6 @@ const TYPES = [
 ];
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 100],
     "subject-case": [2, "always", ["lower-case"]],
